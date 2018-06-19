3344a26ad70012456506a0e5506e2274d38b8169
Joe Cellura <joecellura@Joes-MacBook-Air.local>
2018-06-19 19:36:59 -0400

    Updated SignUp Page

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
+// import { Redirect } from 'react-router-dom'
 import { Route, Link } from 'react-router-dom'
 import logo from '../logo.svg';
 import '../App.css';
