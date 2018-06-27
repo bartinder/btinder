ac8375a84df0b404d2a05a2b81a1ea5f445c4b76
Matt Willson <mattjameswillson@gmail.com>
2018-06-27 02:35:47 -0400

    Nav logo stays central

--- client/src/pages/navbar.js
+++ client/src/pages/navbar.js
@@ -7,75 +7,70 @@ import axios from 'axios';
 import "./navbar.css";
 
 class Navbar extends Component {
-    constructor() {
-        super()
-        this.logout = this.logout.bind(this)
-    }
+  constructor() {
+    super()
+    this.logout = this.logout.bind(this)
+  }
 
-    logout(event) {
-        event.preventDefault()
-        console.log('logging out')
-        axios.post('/api/users/logout').then(response => {
-          console.log(response.data)
-          if (response.status === 200) {
-            this.props.updateUser({
-              loggedIn: false,
-              email: null
-            })
-          }
-          window.location.assign("/login");
-
-        }).catch(error => {
-            console.log('Logout error')
+  logout(event) {
+    event.preventDefault()
+    console.log('logging out')
+    axios.post('/api/users/logout').then(response => {
+      console.log(response.data)
+      if (response.status === 200) {
+        this.props.updateUser({
+          loggedIn: false,
+          email: null
         })
       }
+      window.location.assign("/login");
 
-    render() {
-        const loggedIn = this.props.loggedIn;
-        console.log('navbar render, props: ')
-        console.log(this.props);
-        
-        return (
-            <div>
+    }).catch(error => {
+      console.log('Logout error')
+    })
+  }
 
-                <header className="navbar App-header" id="nav-container">
-                    <div className="col-s4 col-4" >
-                        {loggedIn ? (
-                            <section className="navbar-section">
-                        
-                                <Link to="/discover" className="btn btn-link text-secondary">
-                                    <span className="text-secondary"></span>
-                                    </Link>
+  render() {
+    const loggedIn = this.props.loggedIn;
+    console.log('navbar render, props: ')
+    console.log(this.props);
+    
+    return (
+      <div>
+        <header className="navbar App-header" id="nav-container">
+          <div className="col-s12 col-12">
+            
+            <div id="top-filler"></div>
+            <img src={require("../pictures/logo4.png")} className="App-logo" alt="logo" />
+            
+            {loggedIn ? (
+              <section className="navbar-section">
+                <Link to="/discover" className="btn btn-link text-secondary">
+                  <span className="text-secondary"></span>
+                </Link>
+                <Link to="#" className="btn btn-link text-secondary" onClick={this.logout}>
+                  <span className="text-secondary"></span>
+                </Link>
+              </section>
+            ) : (
+              <section className="navbar-section">
+                <Link to="/" className="btn btn-link text-secondary">
+                  <span className="text-secondary"></span>
+                </Link>
+                <Link to="/login" className="btn btn-link text-secondary">
+                  <span className="text-secondary"></span>
+                </Link>
+                <Link to="/signup" className="btn btn-link">
+                  <span className="text-secondary"></span>
+                </Link>
+              </section>
+            )}
 
-                                <Link to="#" className="btn btn-link text-secondary" onClick={this.logout}>
-                                <span className="text-secondary"></span>
-                                    </Link>
-                                
-                            </section>
-                        ) : (
-                                <section className="navbar-section">
-                                    <Link to="/" className="btn btn-link text-secondary">
-                                        <span className="text-secondary"></span>
-                                        </Link>
-                                    <Link to="/login" className="btn btn-link text-secondary">
-                                    <span className="text-secondary"></span>
-				                        </Link>
-                                    <Link to="/signup" className="btn btn-link">
-                                    <span className="text-secondary"></span>
-				                        </Link>
-                                </section>
-                            )}
-                    </div>
-                    <div className="col-s4 col-4 col-mr-auto">
-                    <div id="top-filler"></div>
-                        <img src={require("../pictures/logo4.png")} className="App-logo" alt="logo" />
-                    </div>
-                </header>
-            </div>
-
-        );
-
-    }
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default Navbar
