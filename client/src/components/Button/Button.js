0b1b8a5f615dc7d1e53fd8083faec083634732c6
Matt Willson <mattjameswillson@gmail.com>
2018-06-28 03:53:17 -0400

    Purple theme

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -4,8 +4,8 @@ import "./Button.css";
 const Button = (props) => (
 
   <div className="btn-group justify-content-around" role="group" aria-label="...">
-    <button onClick={props.onClick} data-value="skip" id="skip" type="button" className="btn btn-default" {...props}><span data-value="skip" className="fas fa-step-forward"></span></button>
     <button onClick={props.onClick} data-value="dislike" id="dislike" type="button" className="btn btn-default" {...props}><span data-value="dislike" className="fas fa-thumbs-down"></span></button>
+    <button onClick={props.onClick} data-value="skip" id="skip" type="button" className="btn btn-default" {...props}><span data-value="skip" className="fas fa-step-forward"></span></button>
     <button onClick={props.onClick} data-value="like" id="like" type="button" className="btn btn-default" {...props}><span data-value="like" className="fas fa-thumbs-up"></span></button>
   </div>
 
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,13 +5,15 @@ import DropdownButton from "../DropdownButton/DropdownButton.js"
 
 const Footer = (props) => (
   <footer className="footer">
-    <nav className="nav justify-content-around">
-      <div className="row justify-content-around">
-        <button role="presentation" className="nav-item"> <a href="/discover"> <span className="fas fa-beer fa-3X"></span></a></button>
-        <button role="presentation" className="nav-item"> <a href="/friends"><span className="fas fa-users"></span></a></button>
-      <DropdownButton src={props.src} firstName={props.firstName} lastName={props.lastName}/>
-      </div>
-    </nav>
+    <div className="container">
+      <nav className="nav justify-content-around">
+        <div className="row justify-content-around">
+          <button role="presentation" className="nav-item"> <a href="/discover"> <span className="fas fa-beer fa-3X"></span></a></button>
+          <button role="presentation" className="nav-item"> <a href="/friends"><span className="fas fa-users"></span></a></button>
+        <DropdownButton src={props.src} firstName={props.firstName} lastName={props.lastName}/>
+        </div>
+      </nav>
+    </div>
   </footer>
 );
 
