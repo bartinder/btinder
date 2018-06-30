d9f0deaf7af968c11e0a894407116585bee5b5aa
Matt Willson <mattjameswillson@gmail.com>
2018-06-30 06:03:29 -0400

    Friends page styling

--- client/src/components/AddFriend/AddFriend.js
+++ client/src/components/AddFriend/AddFriend.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./AddFriend.css";
 
 const AddFriend = props => (
-  <button onClick={props.onClick.bind(this,props.id)} data-value={props.id} type="button" className="btn btn-default AddFriend">
-  {console.log(JSON.stringify(props) + "addfriend")}
-    <span className="fas fa-plus">
-    </span>
+  <button
+    onClick={props.onClick.bind(this,props.id)}
+    data-value={props.id}
+    type="button"
+    className="btn btn-default AddFriend"
+  >
+    {console.log(JSON.stringify(props) + "addfriend")}
+    <span className="fas fa-plus"></span>
   </button>
-
 );
 
 export default AddFriend;
--- client/src/components/InviteFriend/InviteFriend.js
+++ client/src/components/InviteFriend/InviteFriend.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./InviteFriend.css";
 
 const InviteFriend = props => (
-  <button type="button" className="btn btn-default InviteFriend"><span className="fas fa-phone"></span></button>
+  <button
+    type="button"
+    className="btn btn-default InviteFriend"
+  >
+    <span className="fas fa-phone"></span>
+  </button>
 );
 
 export default InviteFriend;
